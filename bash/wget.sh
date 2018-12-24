b46435267e733f3735e8375d2e3df378389f3a69
happypolo <happypolo.c@gmail.com>
2018-12-24 19:58:23 +0800

    no message

---
+++ bash/wget.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ~/.ncl/lib/ncarg/database/rangs
+wget 'https://www.io-warnemuende.de/tl_files/staff/rfeistel/download/rangs(2).zip'
+wget 'https://www.io-warnemuende.de/tl_files/staff/rfeistel/download/rangs(3).zip'
+wget 'https://www.io-warnemuende.de/tl_files/staff/rfeistel/download/rangs(4).zip'
+wget x
+wget 'https://www.io-warnemuende.de/tl_files/staff/rfeistel/download/gshhs(1).zip'
+wget 'https://www.io-warnemuende.de/tl_files/staff/rfeistel/download/gshhs(2).zip'
+wget 'https://www.io-warnemuende.de/tl_files/staff/rfeistel/download/gshhs(3).zip'
+wget 'https://www.io-warnemuende.de/tl_files/staff/rfeistel/download/gshhs(4).zip'
+exit 0
